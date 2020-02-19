653e534d78dc6d0c5cb4e57a5ca23445ae2214f5
Norio <umanari145@gmail.com>
2020-02-19 23:11:05 +0900

    sqlを投下

---
+++ docker/pgsql/init/start.sql
@@ -0,0 +1,20 @@
+CREATE TABLE persons (
+  id  SERIAL PRIMARY KEY,
+  person_name varchar(50) DEFAULT '',
+  sex smallint DEFAULT NULL,
+  birth_day date DEFAULT NULL,
+  zip varchar(7)  DEFAULT '',
+  address_code varchar(15) DEFAULT '',
+  address1 varchar(100) DEFAULT '',
+  address2 varchar(100) DEFAULT '',
+  contact smallint DEFAULT NULL,
+  email varchar(200) DEFAULT '',
+  tel varchar(15) DEFAULT '',
+  delivery_zip varchar(7) DEFAULT '',
+  delivery_address1 varchar(100) DEFAULT '',
+  delivery_address2 varchar(100) DEFAULT '',
+  traffic json DEFAULT NULL,
+  contents texte,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
